5cff20f6566eb4a471474a385733308a7c22ff9d
Bianchi-Dario <91790218+Bianchi-Dario@users.noreply.github.com>
2021-12-01 18:38:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+//
+// Created by bianc on 01/12/2021.
+//
 
+#include "lib.h"
+bool conversione (char &a){
+    bool t = true;
+    if (a>= 65 & a<=90 or a>=97 &a<=122){
+        t= true;
+        if(a>=65 & a<= 90){
+            a=a+32;
+            return &a;
+        }
+    }else{
+        t=false;
+    }
+    return t;
+}
